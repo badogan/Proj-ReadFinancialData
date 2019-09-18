bd9570eba83b8cbfce097c1114587c95df699d2c
Basri Dogan <badogan@live.com>
2019-09-18 15:58:56 +0100

    after making sure all works which also involved adding packes

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
 '''Started in github.
 And now continuing from my local
 '''
+
+# Import DataReader, Import date, Import matplotlib
+from  pandas_datareader.data import DataReader
+from datetime import date 
+import matplotlib.pyplot as plt
+
+# Set the start date
+start = date(1950,1,1)
+
+# Define the series codes
+series = ['UNRATE', 'CIVPART']
+
+# Import the data
+econ_data = DataReader(series, 'fred', start=start)
+
+# Assign new column labels
+econ_data.columns = ['Unemployment Rate','Participation Rate']
+
+# Plot econ_data
+econ_data.plot(subplots=True, title='Labor Market')
+
+# Show the plot
+plt.show()
+
+
